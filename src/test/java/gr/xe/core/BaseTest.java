72814dd96aab9ba32b8d3f7e8e85f4cd1f622224
markella efthymiou <marmiou@MacBook-Air-markella.local>
2026-02-28 18:48:43 +0200

    Add BaseTest

---
+++ src/test/java/gr/xe/core/BaseTest.java
@@ -0,0 +1,29 @@
+package gr.xe.core;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeMethod(alwaysRun = true)
+    public void setup(String browser) {
+
+        DriverFactory.initDriver(browser);
+
+        driver = DriverFactory.getDriver();
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+
+        DriverFactory.quitDriver();
+
+    }
+
+}
